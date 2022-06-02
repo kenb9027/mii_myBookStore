8b38e749f7591ead41a11e0191108ba1ff9182b8
kenb9027 <kenb9027@gmail.com>
2022-06-02 16:17:29 +0200

    add txt

---
+++ README.md
@@ -0,0 +1,33 @@
+Nouveau Projet Symfony 5.4
+-> MyBookStore
+Dependances Front
+-> Bootstrap
+Entité
+-> book - id - title - description - price - cover - createdAt (valeur par defaut = date de l'insertion) - publishedAt - isActive (defaut = false)
+-> author - id - firstname - lastname - fullname (genaré automatiquement = firstname + lastname) - gender (M, F, N)
+-> category - id - name - description - color (7 caractères) - illustration
+Formulaire
+-> BookType
+-> AuthorType
+-> CategoryType
+Routes
+accueil /
+Liste des livres /books
+Details d'un livre /book/{id}
+Ajouter un livre /book
+Editer un livre /book/{id}/edit
+Supprimer un livre /book/{id}/delete
+Liste des auteurs /authors
+Details d'un auteur /author/{id}
+Ajouter un auteur /author
+Editer un auteur /author/{id}/edit
+Supprimer un auteur /author/{id}/delete
+Liste des catégories /categories
+Details d'une catégorie /category/{id}
+Ajouter une catégorie /category
+Editer une catégorie /category/{id}/edit
+Supprimer une catégorie /category/{id}/delete
+////////////////////////////////////////////////
+-> Relations entre les entités
+-> EntityType et CollectionType dans les formulaires
+-> Sécurité (restriction d'accès)
---
+++ src/Entity/Book.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 180)]
+    private $title;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $description;
+
+    #[ORM\Column(type: 'string', length: 180, nullable: true)]
+    private $cover;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $createdAt;
+
+    #[ORM\Column(type: 'datetime_immutable', nullable: true)]
+    private $publishedAt;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isActive;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCover(): ?string
+    {
+        return $this->cover;
+    }
+
+    public function setCover(?string $cover): self
+    {
+        $this->cover = $cover;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getPublishedAt(): ?\DateTimeImmutable
+    {
+        return $this->publishedAt;
+    }
+
+    public function setPublishedAt(?\DateTimeImmutable $publishedAt): self
+    {
+        $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+
+    public function isIsActive(): ?bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): self
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+}
